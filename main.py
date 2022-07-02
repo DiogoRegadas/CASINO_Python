adbc2cfc66b472146897ce757c35fae9f7214f45
Diogo Regadas <107991210+DiogoRegadas@users.noreply.github.com>
2022-07-02 20:43:13 +0100

    ADD MAIN
    Upload main

---
+++ main.py
@@ -0,0 +1,21 @@
+Firstname = input("Firstname : ")
+Lastname = input("Lastname : ")
+Age = int(input("Age : "))
+cont = ""
+
+
+def a_function(first, last, age):
+    if 18 > age:
+        print("Hi, ", first, last, ", comeback when you have 18")
+    else:
+        print("Hi, ", first, last, ", do you like to continue ?")
+        result = input("Yes or No ? : ")
+        result.upper()
+        return result
+
+
+eu = a_function(Firstname, Lastname, Age)
+
+print(eu)
+
+
