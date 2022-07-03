8b1ab3263fdbccc7b9551ac1ad1c2642055a6ddb
Diogo Regadas <Diogo.Regadas@outlook.pt>
2022-07-03 21:13:39 +0100

    Creation of more functions
    Enable the possibility to enter Insert coin menu and leave it

--- main.py
+++ main.py
@@ -1,77 +1,126 @@
-Firstname = input("Firstname : ")
-Lastname = input("Lastname : ")
-Age = int(input("Age : "))
-y = "YES"
-n = "NO"
-f = "False"
-t = "True"
+Firstname = input("Firstname : ")  # Firstname user
+Lastname = input("Lastname : ")  # Lastname user
+Age = int(input("Age : "))  # Age user
+y = "YES"  # var y represents YES
+n = "NO"  # var n represents
+selectmenu: int = 1  # Menu selection
 
 
 def a_function(first, last, age):
-    if 18 > age:
+    if 18 > age:  # if age lower then 18
         print("Hi, ", first, last, ", comeback when you have 18")
         d_function()
-    else:
+    else:  # if age higher then 18
         print("Hi, ", first, last, ", do you like to continue ?")
-        result = input("Yes or No ? : ")
+        result = input("Yes or No ? : ")  # if you want to leave or stay
 
-        while result.upper() != y and result.upper() != n:
+        while result.upper() != y and result.upper() != n:  # if input result not equals yes or no, repeat input
             result = input("Yes or No ? : ")
         return result
 
 
 def b_function(text, stat1):
-    if text == stat1:
+    if text == stat1:  # If says yes from a_function then return True
         result: bool = True
-    else:
+    else:  # If says no from a_function then return False
         result: bool = False
     return result
 
 
-def c_function():
+def c_function():  # Main Menu design
     print("-----------------------")
     print("        CASINO         ")
     print("-----------------------")
     print(" ")
     print("Insert Coins - Press 1")
     print("Gambling - Press 2")
-    print("Coins - Press 3")
+    print("My Coins - Press 3")
     print("Exit - Press 4")
     print(" ")
     print("-----------------------")
 
 
-def d_function():
+def d_function():  # Exit program function
     print("See you next time")
     exit()
 
 
 def e_function(result):
-    if result:
+    if result:  # If b_function result equals True then initial c_function
         c_function()
+        global selectmenu
+        selectmenu = 1  # var selectmenu equals 1 (main menu)
         choose = input("Choose : ")
-        return choose
-    else:
-        d_function()
+        return choose  # return option choose by user (1, 2, 3 or 4)
+    else:  # If b_function result equals False then initial c_function
+        d_function()  # exit
 
 
 def f_function(choose):
+    match choose:  # Check Option Choose by the user and return the respective function
+        case '1':
+            g_function()  # initial design Insert Coin menu
+            global selectmenu
+            selectmenu = 2  # var selectmenu equals 2 (Insert Coin menu)
+            choose = input("Choose : ")
+            return menu_function(selectmenu, choose)  # returns function to user input "choose" do the respective
+            #                                           menu functions
+        case '2':
+            return print("Gambling")  # Check Option Choose by the user and return the respective function
+        case '3':
+            return print("Coins")  # Check Option Choose by the user and return the respective function
+        case '4':
+            return d_function()  # exit
 
-    match choose:
-        case 1:
-            "Insert Coins"
-        case 2:
-            "Gambling"
-        case 3:
-            "Coins"
-        case 4:
-            return d_function()
+
+def g_function():  # Insert Coin Menu design
+    print("-----------------------")
+    print(" CASINO - Insert Coin  ")
+    print("-----------------------")
+    print(" ")
+    print("10000 Coins - Press 1")
+    print("1000 Coins - Press 2")
+    print("100 Coins - Press 3")
+    print("Back - Press 4")
+    print(" ")
+    print("-----------------------")
 
 
-resultA = a_function(Firstname, Lastname, Age)
+def menu_function(selectedmenu, choose):
+    if selectedmenu == 2:  # Check what menu was entered
+        match choose:  # Check Option Choose by the user and return the respective function
+            case '1':
+                return print("Gambling")  # Check Option Choose by the user and return the respective function
+            case '2':
+                return print("Gambling")  # Check Option Choose by the user and return the respective function
+            case '3':
+                return print("Coins")  # Check Option Choose by the user and return the respective function
+            case '4':
+                return backToMain()  # Back to main menu
+    elif selectedmenu == 3:
+        match choose:  # Check Option Choose by the user and return the respective function
+            case '1':
+                return print("Gambling")  # Check Option Choose by the user and return the respective function
+            case '2':
+                return print("Gambling")  # Check Option Choose by the user and return the respective function
+            case '3':
+                return print("Coins")  # Check Option Choose by the user and return the respective function
+            case '4':
+                return backToMain()  # Back to main menu
 
-resultB = b_function(resultA.upper(), y)
 
-select = e_function(resultB)
+def backToMain():  # Back to main menu
+    c_function()  # Recreate main menu
+    global selectmenu
+    selectmenu = 1  # Assign selectmenu equals 2 (Insert Coin menu)
+    choose = input("Choose : ")
+    return f_function(choose)  # returns f_function to user input "choose" do main menu functions
 
-f_function(select)
+
+resultA = a_function(Firstname, Lastname, Age)  # Check Age
+
+resultB = b_function(resultA.upper(), y)  # Check if user want to continue
+
+optionChoose = e_function(resultB)  # if user say yes continue, if no End program
+
+f_function(optionChoose)  # Program check what option the user made (1, 2, 3 or 4)
